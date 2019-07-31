96bf03f7cb26e94cd3a32504a85beb9b158cac7f
Renyi Tan <renyi@getoutlaw.com>
2019-07-31 16:32:44 -0400

    HOLY SHIT I MADE IT WORK

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,19 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
+
+  const presets = [
+    'babel-preset-expo',
+    "module:metro-react-native-babel-preset"
+  ];
+
+  const plugins = [
+    "@babel/plugin-transform-flow-strip-types",
+    ["@babel/plugin-proposal-class-properties", { "loose": true }]
+  ]
+
   return {
-    presets: ['babel-preset-expo']
-  };
+    presets,
+    plugins,
+    'sourceMaps': true
+  }
 };
