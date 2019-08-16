3d1821fde45e8efb08f555b562779681a060f775
xhxh96 <xianhao@u.nus.edu>
2019-08-16 01:06:22 -0400

    Fixed routing of screens - Default screen is now `Welcome` screen - Selecting login via email will bring user to `AuthPage` navigator

--- src/containers/Welcome/Welcome.tsx
+++ src/containers/Welcome/Welcome.tsx
@@ -3,13 +3,24 @@ import { ImageBackground, SafeAreaView, Text, View } from 'react-native';
 import Button from '../../components/Button';
 import { DEVICE_WIDTH } from '../../constants/deviceDimensions';
 import styles from './styles';
+import {
+  NavigationParams,
+  NavigationRoute,
+  NavigationScreenProp
+} from 'react-navigation';
+import { AUTH_PAGE } from '../../constants/routeKeys';
 
-class Welcome extends React.Component {
+interface Props {
+  navigation: NavigationScreenProp<NavigationRoute, NavigationParams>;
+}
+
+class Welcome extends React.Component<Props> {
   static navigationOptions = {
     header: null
   };
 
   render() {
+    const { navigation } = this.props;
     return (
       <View style={styles.container}>
         <ImageBackground
@@ -18,27 +29,20 @@ class Welcome extends React.Component {
         >
           <SafeAreaView style={styles.contentContainer}>
             <View style={styles.sectionContainer}>
-              <Text
-                style={styles.headerText}
-              >
-                Welcome to SIApp
-              </Text>
+              <Text style={styles.headerText}>Welcome to SIApp</Text>
             </View>
 
             <View
               style={[styles.sectionContainer, { width: DEVICE_WIDTH * 0.8 }]}
             >
-              <Text
-                style={[styles.contentText, {marginBottom: 30}]}
-              >
+              <Text style={[styles.contentText, { marginBottom: 30 }]}>
                 The best way to navigate your world and discover
               </Text>
-              <Text
-                style={styles.contentText}
+              <Text style={styles.contentText}>CONTINUE WITH:</Text>
+              <Button
+                buttonStyle={{ marginTop: 10 }}
+                onPress={() => navigation.navigate(AUTH_PAGE)}
               >
-                CONTINUE WITH:
-              </Text>
-              <Button buttonStyle={{ marginTop: 10 }} onPress={() => null}>
                 EMAIL
               </Button>
               <Button
